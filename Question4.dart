1f6ac6a1fd6fc7e41b38cca72b66a973e15b4426
Tamzid-Chy <cse_0182320012101173@lus.ac.bd>
2025-10-27 01:09:41 +0600

    Add files via upload

---
+++ Question4.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> days = [];
+  days.addAll(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]);
+  print("Days of the week:");
+  days.forEach(print);
+}
